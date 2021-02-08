a717dde3ce1486cbb864be73f7c0a3dedc6c4b0d
PeterPwn <peterpwn247@googlemail.com>
2021-02-08 13:24:18 +0100

    fixed resetting on mondays after 9

--- fnc/src/div2xp/getResetDay.js
+++ fnc/src/div2xp/getResetDay.js
@@ -6,11 +6,10 @@ const getTime = require(`${require.main.path}/fnc/src/getTime.js`);
 
 //get the div2 xp reset day of the week
 module.exports = function(date = moment()) {
-	if (date instanceof moment) {
-		if (date.isoWeekday() <= CON.DIV2XPRST.DAY && getTime(date).isBefore(getTime([CON.DIV2XPRST.HOUR, CON.DIV2XPRST.MINUTE]))) {
-			date.subtract(1, 'w');
-		}
-		date.isoWeekday(CON.DIV2XPRST.DAY).hour(CON.DIV2XPRST.HOUR).minute(CON.DIV2XPRST.MINUTE);
+	if (!(date instanceof moment)) date = moment();
+	if (date.isoWeekday() < CON.DIV2XPRST.DAY || date.isoWeekday() == CON.DIV2XPRST.DAY && getTime(date).isBefore(getTime([CON.DIV2XPRST.HOUR, CON.DIV2XPRST.MINUTE]))) {
+		date.subtract(1, 'w');
 	}
+	date.isoWeekday(CON.DIV2XPRST.DAY).hour(CON.DIV2XPRST.HOUR).minute(CON.DIV2XPRST.MINUTE);
 	return date;
 };
