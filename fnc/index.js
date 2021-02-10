ec1284fcb0e6287652910e2265665753b51db82a
PeterPwn <peterpwn247@googlemail.com>
2021-02-10 03:09:38 +0100

    made circular module dependency possible

--- fnc/index.js
+++ fnc/index.js
@@ -1,21 +1,25 @@
 //load functions from disc
 const fs = require('fs');
 
-const path = `${module.path}/src`;
-const functions = {};
-fs.readdirSync(path, { withFileTypes: true }).filter(dirent => dirent.isDirectory() || dirent.name.endsWith('.js')).forEach(dirent => {
-	if (dirent.isDirectory()) {
-		const dir = fs.readdirSync(`${path}/${dirent.name}`).filter(file => file.endsWith('.js'));
-		if (dir.length > 0) {
-			functions[dirent.name] = {};
-			dir.forEach(file => {
-				functions[dirent.name][file.slice(0, -3)] = require(`${path}/${dirent.name}/${file}`);
-			});
-		}
-	}
-	else {
-		functions[dirent.name.slice(0, -3)] = require(`${path}/${dirent.name}`);
-	}
-});
+const logger = require(`${require.main.path}/src/logger.js`);
 
-module.exports = functions;
+const path = `${module.path}/src`;
+try {
+	fs.readdirSync(path, { withFileTypes: true }).filter(dirent => dirent.isDirectory() || dirent.name.endsWith('.js')).forEach(dirent => {
+		if (dirent.isDirectory()) {
+			const dir = fs.readdirSync(`${path}/${dirent.name}`).filter(file => file.endsWith('.js'));
+			if (dir.length > 0) {
+				module.exports[dirent.name] = {};
+				dir.forEach(file => {
+					module.exports[dirent.name][file.slice(0, -3)] = require(`${path}/${dirent.name}/${file}`);
+				});
+			}
+		}
+		else {
+			module.exports[dirent.name.slice(0, -3)] = require(`${path}/${dirent.name}`);
+		}
+	});
+}
+catch (e) {
+	logger.error(`Couldn't load funktions:\n${e.stack}`);
+}
